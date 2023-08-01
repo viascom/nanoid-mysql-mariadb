e28e2ea86ac0d559f687b28e9d4fca575c33589e
Nikola Stankovic <nikola.stankovic@viascom.ch>
2023-08-01 17:31:14 +0200

    feat: initial port to mysql + mariadb

---
+++ README.md
@@ -0,0 +1,70 @@
+# Nano ID for MySQL / MariaDB
+
+_Inspired by the following parent project: [ai/nanoid](https://github.com/ai/nanoid)_
+
+<img src="./logo.svg" align="right" alt="Nano ID logo by Anton Lovchikov" width="180" height="94">
+
+A tiny, secure, URL-friendly, unique string ID generator for MySQL / MariaDB.
+
+> “An amazing level of senseless perfectionism,
+> which is simply impossible not to respect.”
+
+* **Small.** Just a simple MySQL/MariaDB function.
+* **Safe.** It uses pgcrypto random generator.
+* **Short IDs.** It uses a larger alphabet than UUID (`A-Za-z0-9_-`).
+  So ID size was reduced from 36 to 21 symbols.
+
+## Use
+```sql
+SELECT nanoid(); -- creates an id, with the defaults of the created nanoid() function.
+SELECT nanoid_simple(15); -- size parameter set to return 15 digit ids only
+SELECT nanoid_custom(3, 'abcdefghij'); -- custom size and alphabet parameters defined. nanoid() generates ids concerning them
+```
+
+```sql
+DELIMITER $$
+
+DROP TABLE IF EXISTS mytable;
+CREATE TABLE mytable
+(
+    id       VARCHAR(21) PRIMARY KEY,
+    a_column TEXT NOT NULL
+);
+
+CREATE TRIGGER generate_nanoid
+    BEFORE INSERT
+    ON mytable
+    FOR EACH ROW
+BEGIN
+    IF NEW.id IS NULL OR NEW.id = '' THEN
+        SET NEW.id = nanoid();
+        -- OR: SET NEW.id = nanoid_simple(21);
+        -- OR: SET NEW.id = nanoid_custom(21, '_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    END IF;
+END;
+$$
+DELIMITER ;
+
+INSERT INTO mytable (a_column)
+VALUES ('Hi :)');
+
+SELECT *
+FROM mytable;
+```
+
+## Getting Started
+
+Execute the file `nanoid.sql` to create the `nanoid()` function on your defined schema. The nanoid() function will only be available in the specific database where you run the SQL code provided.
+
+**Manually create the function in each database:** You can connect to each database and create the function. This function can be created manually or through a script if you have many databases. Remember to manage updates to the function. If you change the function in one database, those changes will only be reflected in the other databases if you update each function.
+
+## Authors 🖥️
+
+* **Patrick Bösch** - *Initial work* - [itsmefox](https://github.com/itsmefox)
+* **Nikola Stanković** - *Initial work* - [nik-sta](https://github.com/nik-sta)
+
+See also the list of [contributors](https://github.com/viascom/nanoid-mysql-mariadb/contributors) who participated in this project. 💕
+
+## License
+
+This project is licensed under the Apache License 2.0 - see the [LICENSE](LICENSE) file for details.
---
+++ nanoid.sql
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2023 Viascom Ltd liab. Co
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+DELIMITER $$
+
+CREATE OR REPLACE FUNCTION nanoid_custom(
+    size INT,
+    alphabet TEXT
+)
+    RETURNS TEXT
+    LANGUAGE SQL
+    NOT DETERMINISTIC
+    SQL SECURITY INVOKER
+BEGIN
+    DECLARE idBuilder TEXT DEFAULT '';
+    DECLARE counter INT DEFAULT 0;
+    DECLARE alphabetIndex INT;
+    DECLARE alphabetArray VARCHAR(64);
+    DECLARE alphabetLength INT;
+    DECLARE mask INT;
+    DECLARE step INT;
+
+    SET size = IFNULL(size, 21);
+    SET alphabet = IFNULL(alphabet, '_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+    SET alphabetArray = alphabet;
+    SET alphabetLength = CHAR_LENGTH(alphabet);
+    SET mask = POW(2, FLOOR(LOG(alphabetLength - 1) / LOG(2))) - 1;
+    SET step = CEILING(1.6 * mask * size / alphabetLength);
+
+    create_loop:
+    WHILE CHAR_LENGTH(idBuilder) < size
+        DO
+            SET counter = 0;
+            add_character:
+            WHILE counter < step
+                DO
+                    SET alphabetIndex = MOD(CONV(HEX(RANDOM_BYTES(1)), 16, 10), alphabetLength) + 1;
+                    IF alphabetIndex <= alphabetLength THEN
+                        IF CHAR_LENGTH(idBuilder) < size THEN
+                            SET idBuilder = CONCAT(idBuilder, SUBSTRING(alphabetArray, alphabetIndex, 1));
+                        ELSE
+                            LEAVE create_loop;
+                        END IF;
+                    END IF;
+                    SET counter = counter + 1;
+                END WHILE;
+        END WHILE;
+
+    RETURN idBuilder;
+END
+$$
+
+CREATE OR REPLACE FUNCTION nanoid_simple(
+    size INT
+)
+    RETURNS TEXT
+    LANGUAGE SQL
+    NOT DETERMINISTIC
+    SQL SECURITY INVOKER
+BEGIN
+    SET size = IFNULL(size, 21);
+    RETURN nanoid_custom(size, '_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+END
+$$
+
+CREATE OR REPLACE FUNCTION nanoid()
+    RETURNS VARCHAR(21)
+    LANGUAGE SQL
+    NOT DETERMINISTIC
+    SQL SECURITY INVOKER
+BEGIN
+    RETURN nanoid_simple(21);
+END
+$$
+
+DELIMITER ;
